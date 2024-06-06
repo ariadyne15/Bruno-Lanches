5b6b4357e89f08ef60b715682991bac3d44d7cc1
Ariadyne Doval <ariadyne.rd@gmail.com>
2024-06-06 20:38:31 -0300

    criado o gitignore

--- backend/controllers/ClienteController.js
+++ backend/controllers/ClienteController.js
@@ -1,5 +1,6 @@
 const cliente = require('../models/Cliente')
 const bcrypt = require('bcrypt')
+const criarClienteToken = require("../helpers/cria-cliente-token")
 
 module.exports = class ClienteController{
     static async registrar(req, res){ 
@@ -29,5 +30,27 @@ module.exports = class ClienteController{
             res.status(422).json({mensagem: "Confirme a senha"})
             return
         }
-    }
+    
+        /* VERIFICA SE O CLIENTE JÁ ESTA CADASTRADO */
+        const clienteExiste = await Cliente.findOne ({email:email})
+
+        if (clienteExiste){
+            res.status(422).json({mensagem: "E-mail ja cadastrado"})
+            resturn
+        }
+        
+        /* CRIAÇÃO DE SENHA */
+       const salt = await bcrypt.genSalt(12)
+       const senhaHash = await bcrypt.hash(senha,salt)
+
+       /* ADICIONANDO O CLIENTE AO BD */
+       const cliente = new Cliente({nome, email, telefone, senha: senhaHash})
+
+        try{
+            const novoCliente = await cliente.save()
+            await criarClienteToken(novoCliente, req, res)
+        } catch(erro){
+            res.status(500).json({mensagem: erro})
+        }
+    } /* FIM DO MÉTODO REGISTRAR */
 }
